2a4fe62c4f40e1520665ebe3f8546651ebe66a72
Igor Sidorov <igor.sidorov@onapp.com>
2019-08-13 14:22:50 +0300

    use safe bash options

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-set -e
+set -Eeuo pipefail
 
 while true
 do
-  case "$1" in
+  case "${1-}" in
     -r | --ruby )
       shift
       RUBY_VERSION=$1
@@ -41,7 +41,9 @@ do
   esac
 done
 
-if [ -z "$RUBY_VERSION" ]
+set -x
+
+if [ -z "${RUBY_VERSION-}" ]
 then
   echo "RUBY_VERSION is empty"
   exit 1
@@ -50,26 +52,26 @@ fi
 RUBY_INSTALL_VERSION="0.7.0"
 
 PARENT_VERSION=${DEBIAN_VERSION:-buster}
-PARENT_IMAGE="debian:$DEBIAN_VERSION"
+PARENT_IMAGE="debian:$PARENT_VERSION"
 
 NAME="ruby-$RUBY_VERSION"
 VERSION="2"
-TAG="$VERSION-$DEBIAN_VERSION"
+TAG="$VERSION-$PARENT_VERSION"
 
 cleanup() {
-  if [ "$build_mount" != "" ]; then
+  if [ -n "${build_mount:-}" ]; then
     buildah unmount $build_container || true
     build_mount=""
   fi
-  if [ "$mount" != "" ]; then
+  if [ -n "${mount:-}" ]; then
     buildah unmount $container || true
     mount=""
   fi
-  if [ "$build_container" != "" ]; then
+  if [ -n "${build_container:-}" ]; then
     buildah rm $build_container || true
     build_container=""
   fi
-  if [ "$container" != "" ]; then
+  if [ -n "${container:-}" ]; then
     buildah rm $container || true
     container=""
   fi
@@ -80,7 +82,8 @@ trap cleanup EXIT INT TERM
 build_container=$(buildah from $PARENT_IMAGE)
 
 # dependencies
-buildah run $build_container sh -c '
+buildah run $build_container bash -c '
+set -Eeuxo pipefail
 DEBIAN_FRONTEND=noninteractive apt-get -qq update
 DEBIAN_FRONTEND=noninteractive apt-get -qq install -y --no-install-recommends \
   build-essential \
@@ -89,24 +92,28 @@ DEBIAN_FRONTEND=noninteractive apt-get -qq install -y --no-install-recommends \
 '
 
 # ruby-install
-buildah run $build_container sh -c "
+buildah run $build_container bash -c "
+set -Eeuxo pipefail
 cd /tmp
 wget -O ruby-install-$RUBY_INSTALL_VERSION.tar.gz https://github.com/postmodern/ruby-install/archive/v$RUBY_INSTALL_VERSION.tar.gz
 tar -xzvf ruby-install-$RUBY_INSTALL_VERSION.tar.gz
 "
 
 # prevent documentation generation for gems
-buildah run $build_container sh -c "
+buildah run $build_container bash -c "
+set -Eeuxo pipefail
 echo 'gem: --no-document' > /root/.gemrc
 "
 
 # ruby
-buildah run $build_container sh -c "
+buildah run $build_container bash -c "
+set -Eeuxo pipefail
 /tmp/ruby-install-$RUBY_INSTALL_VERSION/bin/ruby-install --system ruby $RUBY_VERSION -- --disable-install-doc
 "
 
 # cleanup
-buildah run $build_container sh -c "
+buildah run $build_container bash -c "
+set -Eeuxo pipefail
 DEBIAN_FRONTEND=noninteractive apt-get -qq purge -y \
   build-essential \
   ca-certificates \
